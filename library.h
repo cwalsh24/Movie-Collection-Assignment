245a806f0dc989977bbac77435244284a20dcf71
Connor Walsh <cwalsh24@ut.utm.edu>
2022-10-20 13:25:07 -0500

    Current header file

--- library.h
+++ library.h
@@ -12,11 +12,11 @@
 #define LIBRARY_H
 
 #include <string>
+#include <list>
 
 class Library{
  private:
   struct movie{
-    entry* next;
     std::string title;
     std::string director;
     int runtime;
@@ -24,20 +24,19 @@ class Library{
     float price;
     int year;
   };
-  entry* head;
+  std::list<movie> collection;
  public:
   Library();
   ~Library();
-  void destructor_helper(entry* node);
   void read_from_file(std::string fileName);
-  void push_front(std::string title, std::string director, int runtime, std::string format, float price, int year);
-  void push_back(std::string title, std::string director, int runtime, std::string format, float price, int year);
-  void write_to_file(std::string fileName);
-  std::string find_movie(std::string title);
-  std::string director_search(std::string name);
-  void insert_sorted(std::string title, std::string director, int runtime, std::string format, float price, int year);
+  //void push_front(std::string title, std::string director, int runtime, std::string format, float price, int year);
+  //void push_back(std::string title, std::string director, int runtime, std::string format, float price, int year);
+  //void write_to_file(std::string fileName);
+  //std::string find_movie(std::string title);
+  //std::string director_search(std::string name);
+  //void insert_sorted(std::string title, std::string director, int runtime, std::string format, float price, int year);
   void print();
-  void remove(std::title);
+  //void remove(std::title);
 };
 
 #endif //LIBRARY_H
